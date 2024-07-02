733a34ae216fc3403f6a32ba74dafb971e082cdf
Irvan Av <irvanav28@gmail.com>
2024-07-02 21:41:51 +0700

    add database

---
+++ app/Database/Migrations/2024-07-02-142406_CreateProductsTable.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateProductsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'user_id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+            ],
+            'title' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'price' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '10,2',
+            ],
+            'quantity' => [
+                'type'       => 'SMALLINT'
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+                'default' => NULL
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+                'on update' => "CURRENT_TIMESTAMP"
+            ]
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('user_id', 'users', 'id');
+        $this->forge->createTable('products');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('products');
+    }
+}
